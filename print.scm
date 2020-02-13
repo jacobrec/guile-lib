24655e650b1e7f1dcc069398b988ad52a0765321
Jacob Reckhard <jacobreckhard@gmail.com>
2020-02-13 10:48:26 -0700

    added printing with color macros

--- print.scm
+++ print.scm
@@ -1,5 +1,13 @@
 (define-module (jlib print)
-  #:export (println))
+  #:export (println
+            with-color
+            with-foreground
+            with-background))
+
+(define colors
+  '((#:BLK . 0) (#:RED . 1) (#:GRN . 2) (#:YEL . 3)
+    (#:BLU . 4) (#:MAG . 5) (#:CYN . 6) (#:WHT . 7)
+    (#:RST . 9)))
 
 (define (println . items)
   (format #t "~{~A~^ ~}~%" items))
@@ -15,4 +23,32 @@
     (apply inner rest)))
 
 
-(println 1 2 4)
+(define-macro (with-color fg bg fn)
+  `(begin
+     (ansi-code (+ 30 (assoc-ref colors ,fg))
+                (+ 40 (assoc-ref colors ,bg))
+                #\m)
+     ,fn
+     (ansi-code (+ 30 (assoc-ref colors #:RST))
+                (+ 40 (assoc-ref colors #:RST))
+                #\m)))
+
+(define-macro (with-foreground fg fn)
+  `(begin
+     (ansi-code (+ 30 (assoc-ref colors ,fg)) #\m)
+     ,fn
+     (ansi-code (+ 30 (assoc-ref colors #:RST)) #\m)))
+
+(define-macro (with-background bg fn)
+  `(begin
+     (ansi-code (+ 40 (assoc-ref colors ,bg)) #\m)
+     ,fn
+     (ansi-code (+ 40 (assoc-ref colors #:RST)) #\m)))
+
+; Usage example
+; (with-color #:YEL #:BLU (display "Hello, World!"))
+; (println)
+; (with-foreground #:BLU (display "Hello, World!"))
+; (println)
+; (with-background #:BLU (display "Hello, World!"))
+; (println)
